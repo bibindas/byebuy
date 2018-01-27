da1d0690144cba93fa50382699da080710a54319
Bibindas <bibindas021@gmail.com>
2018-01-27 10:04:08 +0530

    Price bug fix

--- bb_backend/views.py
+++ bb_backend/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 def home(request):
 	brand = Brand.objects.all()
 	products = Product.objects.all()
+	n_products = Product.objects.all()
 	colour = Colour.objects.all()
 	brand_name = request.GET.get("brand_name", None)
 	print brand_name
@@ -23,30 +24,42 @@ def home(request):
 		sl_colour = sl_colour.split(",")
 		query_dict["product_colour__colour__in"] = sl_colour
 
-	if sl_type:
-		query_dict["product_type"] = sl_type
-	if sl_price:
-		query_dict["product_price__lt"] = sl_price	
-	print query_dict
 	if query_dict:
-		products = Product.objects.filter(**query_dict)	
+		n_products = Product.objects.filter(**query_dict)	
+
 	if search:
-		products = products.filter(Q(product_brand__brand_name__contains=search)|Q(product_item__item_name__contains=search)|Q(product_name__contains=search))
-	types = []
-	if products:
-		first_product = products[0]
+		n_products = n_products.filter(Q(product_brand__brand_name__contains=search)|Q(product_item__item_name__contains=search)|Q(product_name__contains=search))
+	
+	if n_products:
+		first_product = n_products[0]
 		price_max = first_product.product_price
 		price_min = first_product.product_price
 	else:
 		price_min = 0
 		price_max = 0
-	for i in products:
+	
+	types = []
+
+	for i in n_products:
 		if i.product_price > price_max:
 			price_max = i.product_price
 		if i.product_price < price_min:
 			price_min = i.product_price
 		if i.product_type not in types:
 			types.append(i.product_type)
+
+	if sl_type:
+		query_dict["product_type"] = sl_type
+
+	if sl_price:
+		query_dict["product_price__lte"] = sl_price	
+
+	if query_dict:
+		products = Product.objects.filter(**query_dict)	
+
+	if search:
+		products = products.filter(Q(product_brand__brand_name__contains=search)|Q(product_item__item_name__contains=search)|Q(product_name__contains=search))
+	
 	print types
 	context = {'brand':brand,'types':types,'colour':colour,'products':products}
 	context["brand_name"] = brand_name
@@ -54,6 +67,7 @@ def home(request):
 	context["sl_type"] = sl_type
 	context["price_min"] = price_min
 	context["price_max"] = price_max
-	context["sl_price"] = (price_max+price_min)/2
+	context["sl_price"] = sl_price
 	context["search"] = search
+	context["s_type"] = sl_type
 	return render(request,'byebuy/index.html', context)
